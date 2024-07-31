973961409d9105a8e299754118742028151a6228
armanwork <arman.nb.work@gmail.com>
2024-07-31 19:31:26 +0430

    array structure completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Array arr = new Array(3);
 
-        arr.Insert(10);
-        arr.Insert(20);
-        arr.Insert(30);
-        arr.Insert(40);
-        arr.Insert(40);
-        arr.removeAt(1);
-        arr.print();
+
+//        Array arr = new Array(3);
+//
+//        arr.Insert(10);
+//        arr.Insert(20);
+//        arr.Insert(30);
+//        arr.Insert(40);
+//        arr.Insert(40);
+//        arr.removeAt(1);
+//        arr.print();
+
+//    ArrayList list = new ArrayList();
+//    list.add();
+
+
+
+
+
+
+
     }
-
 }
